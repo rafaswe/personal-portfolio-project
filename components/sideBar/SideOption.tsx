5d3de5d81d8a30930a63ef73b764633400d8d30c
Mahiya Rahman Rafa <49919918+rafaswe@users.noreply.github.com>
2024-01-31 23:10:09 +0600

    Portfolio structure has created.

--- components/sideBar/SideOption.tsx
+++ components/sideBar/SideOption.tsx
@@ -1,7 +1,14 @@
 import { cn } from "@/functions/cn";
+import Image from "next/image";
 
 const SideOption = ({ className }: { className?: string }) => {
-  return <div className={cn(className)}>SideOption</div>;
+  return (
+    <div className={cn(className)}>
+      <button className="px-3">
+        <Image src="/images/file.svg" alt="file icon" width={32} height={32} />
+      </button>
+    </div>
+  );
 };
 
 export default SideOption;
