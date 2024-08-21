f8e740a941abc8e97be41428fc10dfbfc5bd6e68
Mahiya Rafa <mahiya35-2706@diu.edu.bd>
2024-08-21 15:02:10 +0600

    added Metadata

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,8 @@
 import Search from '@/components/showSection/search/Search.container';
-
+import { Metadata } from 'next';
+export const metadata: Metadata = {
+  title: 'Mahiya Rahman Rafa-search',
+};
 const SearchPage = () => {
   return <Search />;
 };
